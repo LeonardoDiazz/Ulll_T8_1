3b4095025e4d37c867cde9c6375e260ba230e02d
LeonardoDiazz <20233tn141@utez.edu.mx>
2025-11-20 22:44:06 -0600

    Corrección de errores, se hizo 5.2 y se agregaron dependencias

---
+++ app/src/main/java/com/example/mediaapp/ViewModel/PlaybackViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.mediaapp.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import mx.edu.utez.grabadormultimedia.data.SettingsRepository
+
+class PlaybackViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PlaybackViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            val repository = SettingsRepository(application)
+            return PlaybackViewModel(application, repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
